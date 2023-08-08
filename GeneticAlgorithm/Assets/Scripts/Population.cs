84c9c67fa80fb330cb8a1c98f63b30c64a70e245
Von-kensington <vonkensington@gmail.com>
2023-08-08 11:02:13 -0500

    Update Population.cs

--- GeneticAlgorithm/Assets/Scripts/Population.cs
+++ GeneticAlgorithm/Assets/Scripts/Population.cs
@@ -1,19 +1,46 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 
 public class Population : MonoBehaviour
 {
     Dot[] population;
     public GameObject dotPrefab;
-    public Transform spawn;
+
     public int popSize;
     // Use this for initialization
     void Start()
     {
-        population = new Dot[popSize];
-        for(int i = 0; i < population.Length; i++)
+        ResetPopulation();
+    }
+
+    private void FixedUpdate()
+    {
+        if (population.Length == 0 || population == null) return;
+        int currentDeadAgents = 0;
+        foreach (Dot dot in population)
         {
-            Instantiate(dotPrefab, spawn.position, Quaternion.identity);
+            currentDeadAgents += dot.DEAD ? 1 : 0;
+        }
+        if(currentDeadAgents == popSize)
+        {
+            ResetPopulation();
+        }
+    }
+
+    void ResetPopulation()
+    {
+        if(population != null)
+        {
+            foreach(Dot dot in population)
+            {
+                Destroy(dot.gameObject);
+            }
+        }
+        population = new Dot[popSize];
+        for (int i = 0; i < population.Length; i++)
+        {
+            population[i] = Instantiate(dotPrefab, transform.position, Quaternion.identity).GetComponent<Dot>();
         }
     }
 }
