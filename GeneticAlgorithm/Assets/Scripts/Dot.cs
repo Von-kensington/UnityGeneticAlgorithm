b46419a63d02dc78d17b08efe9328fb642192961
Von-kensington <vonkensington@gmail.com>
2023-08-08 10:02:14 -0500

    Create Scripts and Create the basic coupling needed

---
+++ GeneticAlgorithm/Assets/Scripts/Dot.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Dot : MonoBehaviour
+{
+    Brain brain;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        brain = new Brain(10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ GeneticAlgorithm/Assets/Scripts/Population.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+
+public class Population : MonoBehaviour
+{
+    Dot[] population;
+    public GameObject dotPrefab;
+    public Transform spawn;
+    public int popSize;
+    // Use this for initialization
+    void Start()
+    {
+        population = new Dot[popSize];
+        for(int i = 0; i < population.Length; i++)
+        {
+            Instantiate(dotPrefab, spawn.position, Quaternion.identity);
+        }
+    }
+}
