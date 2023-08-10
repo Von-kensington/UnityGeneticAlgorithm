efb14d0fbb438d96289ec12059cdbc10fcfaa365
Von-kensington <vonkensington@gmail.com>
2023-08-09 23:23:10 -0500

    Place obstacles and target

---
+++ GeneticAlgorithm/Assets/Scripts/PlaceObstacles.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class PlaceObstacles : MonoBehaviour
+    {
+
+        public GameObject obstacle, target;
+
+
+        // Update is called once per frame
+        void Update()
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                Instantiate(obstacle, Camera.main.ScreenToWorldPoint(Input.mousePosition), Quaternion.identity);
+            }
+            else if (Input.GetMouseButtonDown(1))
+            {
+                target.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            }
+        }
+    }
+}
